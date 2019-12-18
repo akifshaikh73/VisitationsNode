56abf4a4cc94576ee5cbbf7e2bc371f1cc09ab83
Akif Shaikh <akif_s@hotmail.com>
2019-12-18 13:14:17 -0600

    New files

---
+++ visitations.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var chalk = require('chalk');
+var debug=require('debug');
+
+var app=express();
+
+app.get('/',function(req,res){
+    res.send('Visitations Web with Debug');
+});
+
+app.listen(3000,function() {
+    //console.log('listening on '+ chalk.green('3000'));
+    debug(`listening on port ${chalk.green('3000')}`);
+});
