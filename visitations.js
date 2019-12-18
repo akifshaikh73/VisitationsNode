4f417748ed6bff8cf9c5b7c55cf8bfefb23a8b0d
Akif Shaikh <akif_s@hotmail.com>
2019-12-18 16:32:22 -0600

    static

--- visitations.js
+++ visitations.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var chalk = require('chalk');
 var debug=require('debug');
+var morgan=require('morgan');
+var path=require('path');
 
 var app=express();
 
+//app.use(morgan('combined'));
+app.use(morgan('tiny')); // Get logging
+
 app.get('/',function(req,res){
-    res.send('Visitations Web with Debug');
+    res.sendFile(path.join(__dirname ,'views/index.html'));
 });
 
-app.listen(8080,function() {
-    //console.log('listening on '+ chalk.green('3000'));
-    debug('listening on port ${chalk.green(`8080`)}');
+
+app.get('/api',function(req,res) {
+    res.send('Visitations Web Service');
+});
+
+var port = 8080;
+
+app.listen(port,function() {
+    console.log(`listening on ${chalk.green(port)}`);
+    debug(`listening on ${chalk.green(port)}`);
+    //debug('listening on port '+ chalk.green(port));
 });
