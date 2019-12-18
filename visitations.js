0d0f3368819618efddd41d626b78ecf1694d1682
EC2 Default User <ec2-user@ip-172-31-93-250.ec2.internal>
2019-12-18 22:47:32 +0000

    log files

--- visitations.js
+++ visitations.js
@@ -3,15 +3,22 @@ var chalk = require('chalk');
 var debug=require('debug');
 var morgan=require('morgan');
 var path=require('path');
-
+var fs=require('fs');
 var app=express();
 
 //app.use(morgan('combined'));
-app.use(morgan('tiny')); // Get logging
+//app.use(morgan('tiny')); // Get logging
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname ,'views/index.html'));
 });
+
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+//
+// // setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
 
 
 app.get('/api',function(req,res) {
